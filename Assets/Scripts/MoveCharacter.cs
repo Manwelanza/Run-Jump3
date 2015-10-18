e4f3f746b7dc47675d664c70c5f37aaeb6a728c3
Manuel <Manwelanza@gmail.com>
2015-10-18 19:51:17 +0100

    1 commit

---
+++ Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCharacter : MonoBehaviour {
+    public float velocidad = 200f;
+    public float fuerzaSalto = 5f;
+    public float rotateSpeed = 3f;
+    private CharacterController cControl;
+
+    // Use this for initialization
+    void Start () {
+        cControl = GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        //Vector3 direction = new Vector3(0, Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime, 0);
+        //float movementSpeed = Input.GetAxis("Vertical") * velocidad * Time.deltaTime;
+        //transform.Rotate(direction);
+        //cControl.SimpleMove(transform.forward * movementSpeed);
+
+        moveCharacter();
+        jump();
+        cControl.SimpleMove(new Vector3(0, 0, 0)); //Para que continue aplicandose la gravedad
+    }
+
+    void moveCharacter()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        if (horizontal > 0)
+        {
+            cControl.Move(transform.right * velocidad * Time.deltaTime);
+        }
+        if (horizontal < 0)
+        {
+            cControl.Move(-transform.right  * velocidad * Time.deltaTime);
+        }
+        if (vertical > 0)
+        {
+            cControl.Move(transform.forward * velocidad * Time.deltaTime);
+        }
+        if (vertical < 0)
+        {
+            cControl.Move(-transform.forward * velocidad * Time.deltaTime);
+        }
+    }
+
+    void jump()
+    {
+        if (Input.GetAxis("Jump") != 0)
+        {
+            //cControl.SimpleMove(new Vector3(0,1,0) * fuerzaSalto * Time.deltaTime);
+            cControl.Move(new Vector3(0, fuerzaSalto, 0));
+            Debug.Log("si si");
+        }
+    }
+}
+
---
+++ Assets/Scripts/Recolectable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Recolectable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, 60, 0) * Time.deltaTime);
+	}
+}
