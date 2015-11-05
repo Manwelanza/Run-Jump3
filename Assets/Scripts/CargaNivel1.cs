153b43b96692ce4b4f0e9fb76ff3f0760c2ff4c2
José Carlos <joseka1234@gmail.com>
2015-11-05 17:52:29 +0000

    Cambios en el menu

--- Assets/Scripts/CargaNivel1.cs
+++ Assets/Scripts/CargaNivel1.cs
@@ -3,21 +3,34 @@ using System.Collections;
 
 public class CargaNivel1 : MonoBehaviour {
 
-	void OnGUI() {
-		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 4, 200, 50), "Inicio")) {
-			Application.LoadLevel("Nivel01");
-		}
-		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 4 + 100, 200, 50), "Instrucciones")) {
-			// TODO: Hacer que se vean las instrucciones
-		}
-		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 4 + 200, 200, 50), "Creditos")) {
-			// TODO: Hacer que se vean los "Creditos" (nombre de los autores  y tal)
-		}
-		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 4 + 300, 200, 50), "Salir")) {
-			Application.Quit();
-			// TODO: Mirar por que no funciona!
-		}
-
+	public void Awake() {
+		GameObject.Find ("InstruccionesMenu").GetComponent<Canvas> ().enabled = false;
 	}
 
+	public void cargaNivel1() {
+		Application.LoadLevel("Nivel01");
+	}
+	public void salir() {
+		Application.Quit ();
+	}
+	public void mostrarInstrucciones() {
+		GameObject.Find ("InstruccionesMenu").GetComponent<Canvas> ().enabled = true;
+		GameObject.Find ("MenuPrincipal").GetComponent<Canvas> ().enabled = false;
+		
+	}
+
+	public void muestraMenu() {
+		GameObject.Find ("InstruccionesMenu").GetComponent<Canvas> ().enabled = false;
+		GameObject.Find ("MenuPrincipal").GetComponent<Canvas> ().enabled = true;
+	}
+
+	public void entraBoton(GameObject objeto) {
+		objeto.GetComponent<RectTransform> ().SetSizeWithCurrentAnchors (RectTransform.Axis.Horizontal, 250);
+		objeto.GetComponent<RectTransform> ().SetSizeWithCurrentAnchors (RectTransform.Axis.Vertical, 130);
+	}
+
+	public void saleBoton(GameObject objeto) {
+		objeto.GetComponent<RectTransform> ().SetSizeWithCurrentAnchors (RectTransform.Axis.Horizontal, 200);
+		objeto.GetComponent<RectTransform> ().SetSizeWithCurrentAnchors (RectTransform.Axis.Vertical, 100);
+	}
 }
