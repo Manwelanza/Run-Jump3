f34e8c0b23fe0fe28d9a35df92e5249b8b7b6ac5
Manuel <Manwelanza@gmail.com>
2016-01-02 21:49:39 +0000

    start menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Canvas optionMenu;
+    public Button startText;
+    public Button exitText;
+    public Button optionText;
+
+    // Use this for initialization
+    void Start ()
+    {
+        optionMenu = optionMenu.GetComponent<Canvas>();
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        optionText = optionText.GetComponent<Button>();
+        optionMenu.enabled = false;
+        quitMenu.enabled = false;
+    }
+
+    // Método que muestra el menu de opciones y desactiva los botones del menu principal
+    public void OptionPress ()
+    {
+        optionMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+        optionText.enabled = false;
+    }
+
+    // Método que desactiva el menu de opciones y reactiva los botones del menu principal
+    public void OptionDisabled ()
+    {
+        optionMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+        optionText.enabled = true;
+    }
+	
+    // Método que muestra el menu de salir del juego y desactiva los botones del menu principal
+	public void ExitPress ()
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+        optionText.enabled = false;
+    }
+
+    // Método que desactiva el menu de salir del juego y reactiva los botones del menu principal
+    public void NoPress() 
+    {
+        quitMenu.enabled = false;
+        startText.enabled = true; 
+        exitText.enabled = true;
+        optionText.enabled = true;
+    }
+
+    // Método para cargar el primer nivel
+    public void StartLevel() 
+    {
+        Application.LoadLevel("Nivel01"); 
+    }
+
+    // Método para salir del juego
+    public void ExitGame() 
+    {
+        Application.Quit(); 
+    }
+}
